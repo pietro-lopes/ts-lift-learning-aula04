dc8c3779037ae6029cdb43fedb76abfeed34776b
unknownrj <unknownrj@gmail.com>
2022-08-14 03:34:52 -0300

    Add feature deploy task

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,29 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { task } from "hardhat/config";
 import "dotenv/config";
 
 const {
   PRIVATE_KEY,
   POLYGONSCAN_API_KEY,
 } = process.env;
+
+task("deploy", "Deploy contract, use --contract <address> [--args <constructor-args>]'")
+  .addParam("contract", "Contract name")
+  .addOptionalParam("args", "Contract construtor arguments")
+  .setAction(async (taskArgs, hre) => {
+    const [deployer] = await hre.ethers.getSigners();
+
+    console.log(`Deploying contracts with the account:, ${deployer.address}`);
+
+    console.log(`Account balance:, ${(await deployer.getBalance()).toString()}`);
+
+    const Token = await hre.ethers.getContractFactory(taskArgs.contract);
+    //@ts-ignore
+    const token = taskArgs.args ? await Token.deploy(taskArgs.args) : await Token.deploy()
+
+    console.log(`Contract address:, ${token.address}`);
+  });
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
