2fee80e266477f3b179a88290ab4ce7aa613f43b
unknownrj <unknownrj@gmail.com>
2022-08-14 03:18:09 -0300

    Initial config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,29 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
+
+const {
+  PRIVATE_KEY,
+  POLYGONSCAN_API_KEY,
+} = process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {},
+    mumbai: {
+      url: 'https://rpc.ankr.com/polygon_mumbai',
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+    polygon: {
+      url: 'https://rpc.ankr.com/polygon',
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
+  etherscan: {
+    apiKey: POLYGONSCAN_API_KEY
+  },
 };
 
 export default config;
